a84d56da6ef09cd0630f464833d6a23bc9ad736b
Muhammad Umer Mukhtiar <omerjh5004@gmail.com>
2021-12-29 13:35:31 +0500

    ghj

---
+++ COde/COde/src/Model/NewMain.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author Umer
+ */
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+      String a=  LocalDatabase.skils.get(1);
+        System.out.println(LocalDatabase.skils.get(0));
+    }
+    
+}
